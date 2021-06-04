e9ccafcd2a90990dfc5e0e8ab1bd9e191fef3145
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2021-06-04 15:30:25 -0500

    mysqli, NODE: stdin_open: true

--- www/public/index.php
+++ www/public/index.php
@@ -1,2 +1,18 @@
+<?php 
+    phpinfo();
 
-<?php phpinfo() ?>
+    $conn = mysqli_connect("mysql8-container", "root", "secret", "db");
+
+    if (!$conn) {
+        echo "Error: Connection failed." . PHP_EOL;
+        echo "errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    echo "Connected successfully" . PHP_EOL;
+    echo "Host: " . mysqli_get_host_info($conn) . PHP_EOL;
+
+    mysqli_close($conn);
+
+?>
