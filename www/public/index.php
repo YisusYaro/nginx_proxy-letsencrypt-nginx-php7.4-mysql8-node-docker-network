2cb114f7626453446e4d5ea536c4ced47c38a8a1
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2021-12-18 18:49:18 -0600

    using names services without version and new letsencrypt image

--- www/public/index.php
+++ www/public/index.php
@@ -1,7 +1,6 @@
 <?php 
-    phpinfo();
 
-    $conn = mysqli_connect("mysql8-container", "root", "secret", "db");
+    $conn = mysqli_connect("mysql-service", "root", "secret", "db");
 
     if (!$conn) {
         echo "Error: Connection failed." . PHP_EOL;
@@ -10,9 +9,11 @@
         exit;
     }
 
-    echo "Connected successfully" . PHP_EOL;
+    echo "Database connected successfully" . PHP_EOL;
     echo "Host: " . mysqli_get_host_info($conn) . PHP_EOL;
 
     mysqli_close($conn);
+
+    phpinfo();
 
 ?>
